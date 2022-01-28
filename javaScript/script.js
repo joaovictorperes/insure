392ad46bd642253cde39b27293c1376e9c08f3d8
João Victor Peres Silva <vitorr-joao2222@hotmail.com>
2022-01-28 09:57:44 -0300

    accessibility-menu-28-01-22

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,17 @@
 const btnMobile = document.querySelector(".btn-mobile");
 
-function toggleMenu() {
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
   const nav = document.querySelector(".header-nav");
   nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Close menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Open menu");
+  }
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
